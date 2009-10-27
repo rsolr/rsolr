0573e927ecb14d92a223cb3363f712a9e35832bf
Matt Mitchell <goodieboy@gmail.com>
2009-10-27 17:28:16 -0400

    added more specs for net_http connection

--- spec/api/connection/net_http_spec.rb
+++ spec/api/connection/net_http_spec.rb
@@ -1,5 +1,31 @@
 describe RSolr::Connection::NetHttp do
   
-  
+  context '#request' do
+    
+    it 'should forward simple, non-data calls to #get' do
+      http = RSolr::Connection::NetHttp.new
+      http.should_receive(:get).
+        with('/select', :q=>'a').
+          and_return({:status_code=>200})
+      http.request('/select', :q=>'a') 
+    end
+    
+    it 'should forward :method=>:post calls to #post with a special header' do
+      http = RSolr::Connection::NetHttp.new
+      http.should_receive(:post).
+        with('/select', 'q=a', {}, {"Content-Type"=>"application/x-www-form-urlencoded"}).
+          and_return({:status_code=>200})
+      http.request('/select', {:q=>'a'}, :method=>:post)
+    end
+    
+    it 'should forward data calls to #post' do
+      http = RSolr::Connection::NetHttp.new
+      http.should_receive(:post).
+        with("/update", "<optimize/>", {}, {"Content-Type"=>"text/xml; charset=utf-8"}).
+          and_return({:status_code=>200})
+      http.request('/update', {}, '<optimize/>')
+    end
+    
+  end
   
 end
