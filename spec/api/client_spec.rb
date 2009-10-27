558598f59b5e5efd08cc662a812e8335522627ee
Matt Mitchell <goodieboy@gmail.com>
2009-10-27 17:01:33 -0400

    test for method_missing in RSolr::Client

--- spec/api/client_spec.rb
+++ spec/api/client_spec.rb
@@ -1,3 +1,14 @@
 describe RSolr do
   
+  context "method_missing" do
+    
+    it 'a non-existent method should be forwarded to #method_missing and then to #request' do
+      client = RSolr::Client.new nil
+      client.should_receive(:request).
+        with('/music', :q=>'Coltrane')
+      client.music :q=>'Coltrane'
+    end
+    
+  end
+  
 end
