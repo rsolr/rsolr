bf8c1a485b308e33badebcf3cd3b211f4286f824
Matt Mitchell <goodieboy@gmail.com>
2009-10-27 17:17:49 -0400

    added RSolr::Client specs

--- spec/api/client_spec.rb
+++ spec/api/client_spec.rb
@@ -12,12 +12,90 @@ describe RSolr do
   end
   
   context 'update' do
-    it 'should forward /update to #request' do
+    
+    it 'should forward /update to #request("/update")' do
       client = RSolr::Client.new nil
       client.should_receive(:request).
-        with('/update', {}, '</commit>')
+        with('/update', {}, '<commit/>')
       client.update '<commit/>'
     end
+    
+    it 'should forward #add calls to #update' do
+      client = RSolr::Client.new nil
+      client.should_receive(:update).
+        with('<add><doc><field name="id">1</field></doc></add>')
+      client.add :id=>1
+    end
+    
+    it 'should forward #commit calls to #update' do
+      client = RSolr::Client.new nil
+      client.should_receive(:update).
+        with('<commit/>')
+      client.commit
+    end
+    
+    it 'should forward #optimize calls to #update' do
+      client = RSolr::Client.new nil
+      client.should_receive(:update).
+        with('<optimize/>')
+      client.optimize
+    end
+    
+    it 'should forward #rollback calls to #update' do
+      client = RSolr::Client.new nil
+      client.should_receive(:update).
+        with('<rollback/>')
+      client.rollback
+    end
+    
+    it 'should forward #delete_by_id calls to #update' do
+      client = RSolr::Client.new nil
+      client.should_receive(:update).
+        with('<delete><id>1</id></delete>')
+      client.delete_by_id 1
+    end
+    
+    it 'should forward #delete_by_query calls to #update' do
+      client = RSolr::Client.new nil
+      client.should_receive(:update).
+        with('<delete><query>blah</query></delete>')
+      client.delete_by_query 'blah'
+    end
+    
+  end
+  
+  context 'request' do
+    
+    it 'should forward #request calls to the adapter' do
+      client = RSolr::Client.new nil
+      client.adapter.should_receive(:request).
+        with('/music', :q=>'Coltrane', :wt=>:ruby).
+          # empty params so that Client doesn't try to evalulate to Ruby;
+          #   -- this happens if the :wt equal :ruby
+          and_return(:params=>{})
+      client.request '/music', :q=>'Coltrane'
+    end
+    
+  end
+  
+  context 'adapt_response' do
+    
+    it 'should not try to evaluate ruby when the :qt is not :ruby' do
+      client = RSolr::Client.new nil
+      body = '{:time=>"NOW"}'
+      result = client.send(:adapt_response, {:body=>body, :params=>{}})
+      result.should be_a(String)
+      result.should == body
+    end
+    
+    it 'should evaluate ruby responses when the :wt is :ruby' do
+      client = RSolr::Client.new nil
+      body = '{:time=>"NOW"}'
+      result = client.send(:adapt_response, {:body=>body, :params=>{:wt=>:ruby}})
+      result.should be_a(Hash)
+      result.should == {:time=>"NOW"}
+    end
+    
   end
   
 end
