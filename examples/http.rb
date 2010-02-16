7499041309e1196983004b1c182fd8ab6a0659c5
Matt Mitchell <goodieboy@gmail.com>
2010-02-16 16:10:44 -0500

    merged in http://github.com/copiousfreetime/rsolr/tree/update-params merged in mperham branch for adding params to Client #add (4b8c51)

--- examples/http.rb
+++ examples/http.rb
@@ -2,6 +2,9 @@ require File.join(File.dirname(__FILE__), '..', 'lib', 'rsolr')
 
 solr = RSolr.connect
 
+# r = solr.request "/admin/cores", :action => "STATUS"
+# puts r.inspect
+
 Dir['../solr/example/exampledocs/*.xml'].each do |xml_file|
   puts "Updating with #{xml_file}"
   solr.update File.read(xml_file)
