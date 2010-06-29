c5e4cd2b9790bd4260eaecf8e8d192684e3cc6cd
Matt Mitchell <matt@hotelicopter.com>
2010-06-28 21:53:09 -0400

    updated example.rb

--- example.rb
+++ example.rb
@@ -2,11 +2,21 @@ require 'lib/rsolr'
 
 solr = RSolr.connect "http://localhost:9999/solr"
 
+# "asdf" doesn't exists, so a http error is raised...
 begin
-  r = solr.optimize
+  solr.head("asdf")
 rescue
-  puts $!
+  # the error will have #request and #response attributes
+  $!.response[:status] == 404
 end
+
+# "admin" exists so we can check the return value's original response status code
+solr.head("admin").response[:status] == 200
+
+# add some shiz
+solr.add :name_s => "blah blah", :id => Time.now.to_s
+solr.commit
+solr.optimize
 
 begin
   result = solr.get 'select', :q => '*:*'
@@ -23,3 +33,6 @@ begin
 rescue
   puts $!.to_s
 end
+
+solr.delete_by_query "*:*"
+solr.commit
