1c721189670ddb423d5887bea5d61719954a3947
Matt Mitchell <goodieboy@gmail.com>
2011-06-08 22:48:40 -0400

    removed pagination warning... sticking with generic pagination response module

--- lib/rsolr/pagination.rb
+++ lib/rsolr/pagination.rb
@@ -17,10 +17,6 @@ module RSolr::Pagination
     
     # A paginated request method.
     def paginate page, per_page, path, opts = nil
-      @warned ||= begin
-        warn "DEPRECATION WARNING: RSolr::Pagination / pagination functionality will be removed in 1.1.0."
-        true
-      end
       request_context = build_paginated_request page, per_page, path, opts
       execute request_context
     end
@@ -84,7 +80,6 @@ module RSolr::Pagination
     attr_accessor :start, :per_page, :total
 
     # Returns the current page calculated from 'rows' and 'start'
-    # WillPaginate hook
     def current_page
       return 1 if start < 1
       per_page_normalized = per_page < 1 ? 1 : per_page
@@ -92,19 +87,16 @@ module RSolr::Pagination
     end
 
     # Calcuates the total pages from 'numFound' and 'rows'
-    # WillPaginate hook
     def total_pages
       @total_pages ||= per_page > 0 ? (total / per_page.to_f).ceil : 1
     end
 
     # returns the previous page number or 1
-    # WillPaginate hook
     def previous_page
       @previous_page ||= (current_page > 1) ? current_page - 1 : 1
     end
 
     # returns the next page number or the last
-    # WillPaginate hook
     def next_page
       @next_page ||= (current_page == total_pages) ? total_pages : current_page+1
     end
