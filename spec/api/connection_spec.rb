22698161eb975bc518b326af46bad72469afea02
Duncan Robertson <duncan@whomwah.com>
2011-10-06 20:49:19 +0100

    Add transparent support for http_proxy environment variable with tests fix date formatting bug in rake task when using older rubygems

--- spec/api/connection_spec.rb
+++ spec/api/connection_spec.rb
@@ -1,15 +1,67 @@
 require 'spec_helper'
+
 describe "RSolr::Connection" do
-  
-  context "setup_raw_request" do
-    c = RSolr::Connection.new
-    base_url = "http://localhost:8983/solr"
-    client = RSolr::Client.new c, :url => base_url
-    req = c.send :setup_raw_request, {:headers => {"content-type" => "text/xml"}, :method => :get, :uri => URI.parse(base_url + "/select?q=*:*")}
+
+  before do
+    @solr = "http://localhost:8983/solr"
+    @conn = RSolr::Connection.new
+  end
+
+  it "Should handle a raw request" do
+    client = RSolr::Client.new @conn, :url => @solr
+    req = @conn.send :setup_raw_request, {
+      :headers => {"content-type" => "text/xml"}, 
+      :method => :get, 
+      :uri => URI.parse(@solr + "/select?q=*:*")}
     req.path.should == "/solr/select?q=*:*"
     headers = {}
     req.each_header{|k,v| headers[k] = v}
     headers.should == {"content-type"=>"text/xml"}
+  end
+
+  it "handle no proxy passed through" do
+    with_proxy_env nil do
+      req = @conn.send :http, URI::parse(@solr)
+      req.proxyaddr.should == nil 
+      req.proxyport.should == nil 
+    end
+  end
+
+  it "handle a proxy string passed through" do
+    with_proxy_env nil do
+      proxy = "http://www.duncanproxy.com"
+      req = @conn.send :http, URI::parse(@solr), proxy
+      req.proxyaddr.should == "www.duncanproxy.com" 
+      req.proxyport.should == 80 
+    end
+  end
+
+  it "handle a proxy with username and password" do
+    with_proxy_env nil do
+      proxy = "http://duncan:robertson@www.duncanproxy.com"
+      req = @conn.send :http, URI::parse(@solr), proxy
+      req.proxyaddr.should == "www.duncanproxy.com" 
+      req.proxyport.should == 80 
+      req.proxy_user.should == "duncan" 
+      req.proxy_pass.should == "robertson" 
+    end
+  end
+
+  it "handle http_proxy environment set" do
+    with_proxy_env "http://duncan.proxy.com:80" do
+      req = @conn.send :http, URI::parse(@solr)
+      req.proxyaddr.should == "duncan.proxy.com" 
+      req.proxyport.should == 80 
+    end
+  end
+
+  it "handle http_proxy environment set and proxy passed in" do
+    with_proxy_env "http://duncan.proxy.com:80" do
+      proxy = "http://www.duncanproxy.com:8080"
+      req = @conn.send :http, URI::parse(@solr), proxy
+      req.proxyaddr.should == "www.duncanproxy.com" 
+      req.proxyport.should == 8080 
+    end
   end
   
 end
