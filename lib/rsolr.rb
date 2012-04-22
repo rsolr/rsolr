2e4f5017673bbbe0a7a24c3a14311b40dcb736a8
Matt Mitchell <goodieboy@gmail.com>
2012-04-22 16:53:11 -0400

    bumped version

--- lib/rsolr.rb
+++ lib/rsolr.rb
@@ -6,7 +6,7 @@ module RSolr
   
   %W(Response Char Client Error Connection Uri Xml).each{|n|autoload n.to_sym, "rsolr/#{n.downcase}"}
   
-  def self.version; "1.0.7" end
+  def self.version; "1.0.8" end
   
   VERSION = self.version
   
