111e27a9dd2c0cf9c60bebd7e2ec3f94a48a1559
Chris Beer <cabeer@stanford.edu>
2015-02-24 16:49:13 -0800

    Add solr 5 integration scaffolding

--- rsolr.gemspec
+++ rsolr.gemspec
@@ -35,4 +35,5 @@ Gem::Specification.new do |s|
   s.add_development_dependency 'rake', '~> 10.0'
   s.add_development_dependency 'rdoc', '~> 4.0'
   s.add_development_dependency 'rspec', '~> 3.0'
+  s.add_development_dependency 'solr_wrapper'
 end
---
+++ spec/integration/solr5_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+require 'solr_wrapper'
+
+describe "Solr basic_configs" do
+  SOLR_INSTANCE =  SolrWrapper.default_instance({})
+  before(:all) { SOLR_INSTANCE.start }
+  after(:all) { SOLR_INSTANCE.stop }
+
+  context "basic configs" do
+    subject { RSolr.connect url: "http://localhost:#{SOLR_INSTANCE.port}/solr/basic_configs/"}
+    around(:each) do |example|
+      SOLR_INSTANCE.with_collection(name: "basic_configs", dir: File.join(FIXTURES_DIR, "basic_configs")) do |coll|
+        example.run
+      end
+    end
+    describe "HEAD admin/ping" do
+      it "should not raise an exception" do
+        expect { subject.head('admin/ping') }.not_to raise_error
+      end
+
+      it "should not have a body" do
+        expect(subject.head('admin/ping')).to be_nil
+      end
+    end
+  end
+end
