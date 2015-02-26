266aedb37b01b95e021a5619e2649d17ddbe1e37
Naomi Dushay <ndushay@stanford.edu>
2015-02-26 11:02:26 -0800

    add RSolr.solr_escape method and mark RSolr.escape method deprecated as it is incorrect.

--- lib/rsolr.rb
+++ lib/rsolr.rb
@@ -8,7 +8,19 @@ module RSolr
     Client.new driver.new, opts
   end
   
-  # RSolr.escape
+  # RSolr.escape, which is deprecated as of 2015-02
   extend Char
+  
+  # backslash escape characters that have special meaning to Solr query parser
+  # per http://lucene.apache.org/core/4_0_0/queryparser/org/apache/lucene/queryparser/classic/package-summary.html#Escaping_Special_Characters
+  #  + - & | ! ( ) { } [ ] ^ " ~ * ? : \ /
+  # see also http://svn.apache.org/repos/asf/lucene/dev/tags/lucene_solr_4_9_1/solr/solrj/src/java/org/apache/solr/client/solrj/util/ClientUtils.java
+  #   escapeQueryChars method
+  # @return [String] str with special chars preceded by a backslash
+  def self.solr_escape(str)
+    # note that the gsub will parse the escaped backslashes, as will the ruby code sending the query to Solr 
+    # so the result sent to Solr is ultimately a single backslash in front of the particular character 
+    str.gsub(/([+\-&|!\(\)\{\}\[\]\^"~\*\?:\\\/])/, '\\\\\1')
+  end
   
 end
