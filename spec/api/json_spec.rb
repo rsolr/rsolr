df6ff3a194d75d3a7cb1ac56c17eeaad47d7b32a
Chris Beer <chris@cbeer.info>
2016-09-29 09:30:07 -0700

    Add feature-parity between the JSON and XML serializations with additional document attributes

--- spec/api/json_spec.rb
+++ spec/api/json_spec.rb
@@ -31,6 +31,59 @@ RSpec.describe RSolr::JSON do
       message = JSON.parse(generator.add(data), symbolize_names: true)
       expect(message).to eq data
     end
+
+    context 'with add_attr' do
+      it 'should create an add command with the attributes from a hash' do
+        data = {
+          :id=>"1",
+          :name=>'matt'
+        }
+        message = JSON.parse(generator.add(data, boost: 1), symbolize_names: true)
+        expect(message).to include :add
+        expect(message[:add][:doc]).to eq data
+        expect(message[:add][:boost]).to eq 1
+      end
+
+      it 'should create multiple add command with the attributes from a hash' do
+        data = [
+          {
+            :id=>"1",
+            :name=>'matt'
+          },
+          {
+            :id=>"2",
+            :name=>'sam'
+          },
+        ]
+
+        # custom JSON object class to handle Solr's non-standard JSON command format
+        tmp = Class.new do
+          def initialize
+            @source ||= {}
+          end
+
+          def []=(k, v)
+            if k == :add
+              @source[k] ||= []
+              @source[k] << v.to_h
+            elsif v.class == self.class
+              @source[k] = v.to_h
+            else
+              @source[k] = v
+            end
+          end
+
+          def to_h
+            @source
+          end
+        end
+
+        request = generator.add(data, boost: 1)
+        message = JSON.parse(request, object_class: tmp, symbolize_names: true).to_h
+        expect(message[:add].length).to eq 2
+        expect(message[:add].map { |x| x[:doc] }).to eq data
+      end
+    end
   end
 
   it 'should create multiple fields from array values' do
@@ -43,4 +96,41 @@ RSpec.describe RSolr::JSON do
     expect(message.first).to eq data
   end
 
+  describe '#commit' do
+    it 'generates a commit command' do
+      expect(JSON.parse(generator.commit, symbolize_names: true)).to eq(commit: {})
+    end
+  end
+
+  describe '#optimize' do
+    it 'generates a optimize command' do
+      expect(JSON.parse(generator.optimize, symbolize_names: true)).to eq(optimize: {})
+    end
+  end
+
+  describe '#rollback' do
+    it 'generates a rollback command' do
+      expect(JSON.parse(generator.rollback, symbolize_names: true)).to eq(rollback: {})
+    end
+  end
+
+  describe '#delete_by_id' do
+    it 'generates a delete_by_id command for single documents' do
+      expect(JSON.parse(generator.delete_by_id('x'), symbolize_names: true)).to eq(delete: 'x')
+    end
+
+    it 'generates a delete_by_id command for an array of documents' do
+      expect(JSON.parse(generator.delete_by_id(%w(a b c)), symbolize_names: true)).to eq(delete: %w(a b c))
+    end
+  end
+
+  describe '#delete_by_query' do
+    it 'generates a delete_by_id command for single documents' do
+      expect(JSON.parse(generator.delete_by_query('id:x'), symbolize_names: true)).to eq(delete: { query: 'id:x'})
+    end
+
+    it 'generates a delete_by_id command for an array of documents' do
+      expect(JSON.parse(generator.delete_by_id(%w(a b c)), symbolize_names: true)).to eq(delete: %w(a b c))
+    end
+  end
 end
